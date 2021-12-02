ce7c8b36ceff8bf54786ddfafa8286e71ae596cf
boeriksson <bo.sandevind@tele2.com>
2021-12-02 09:01:12 +0100

    Updated part2 - still not correct :(

--- day01/index.js
+++ day01/index.js
@@ -9,11 +9,14 @@ const calcInc = (rowArray, compare) => {
 }
 
 const singleCompare = (rowArray, i) => rowArray[i] > rowArray[i-1] ? 1 : 0
-const getSlide = (rowArray, i) => rowArray[i] + rowArray[i + 1] + rowArray[i + 2]
-const slideCompare = (rowArray, i) => getSlide(rowArray, i) < getSlide(rowArray, i + 1) ? 1 : 0
+const getSlide = (rowArray, i) => parseInt(rowArray[i]) + parseInt(rowArray[i + 1]) + parseInt(rowArray[i + 2])
+const slideCompare = (rowArray, i) => {
+    console.log(`left: ${getSlide(rowArray, i)} right: ${getSlide(rowArray, i + 1)} increase: ${getSlide(rowArray, i) < getSlide(rowArray, i + 1) ? 1 : 0}`)
+    return getSlide(rowArray, i) < getSlide(rowArray, i + 1) ? 1 : 0;
+}
 
 const getLargerMeasurements = (part) =>
     fs.readFile('input.txt', (error, data) =>
         calcInc(data.toString().split('\n'), part === "part1" ? singleCompare : slideCompare))
 
-getLargerMeasurements(process.env.part || "part1")
+getLargerMeasurements(process.env.part || "part2")
